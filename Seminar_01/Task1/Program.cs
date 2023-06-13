ce0099e3af4844c16c4075f8a9e4ccadced22a37
AliskaHas <nhasanova@mail.ru>
2023-06-13 22:52:24 +0300

    Вставила новую папку и выполнила задание 1

---
+++ Seminar_01/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+Console.WriteLine("Введите число: "); //Приглашение пользователя ко вводу числа (вывод в терминал)
+//int number = int.Parse(Console.ReadLine()); так можно конвертировать из строки в число
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+System.Console.WriteLine("Квадрат введеного числа равен: " + result); //можно так выводить, это неформатированная строка
+System.Console.WriteLine($"Квадрат введеного числа равен: {result} вот так вот круто");
